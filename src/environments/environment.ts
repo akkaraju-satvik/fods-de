446c7b35c7a92191aa5e53a8c834f76f5546f1dc
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-03-23 00:36:40 +0530

    project initial config

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'project-fods',
+    appId: '1:474745128496:web:5132a7fe28f48884928ff5',
+    storageBucket: 'project-fods.appspot.com',
+    locationId: 'asia-south1',
+    apiKey: 'AIzaSyDw7P2NaCXpvr-e5sXi4cw5ErA0ny7HMeE',
+    authDomain: 'project-fods.firebaseapp.com',
+    messagingSenderId: '474745128496',
+  },
+  production: false,
+}
