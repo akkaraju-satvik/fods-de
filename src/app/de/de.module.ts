b9aadf277bd82a864b260d97b123b8d75a62c889
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-03-25 19:07:02 +0530

    setup modules

---
+++ src/app/de/de.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DeRoutingModule } from './de-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DeRoutingModule
+  ]
+})
+export class DeModule { }
