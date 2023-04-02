f619a2f8617191cac4a34894a873af7dcbd0ab3d
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-04-02 20:48:38 +0530

    phone auth

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+declare global {
+  interface Window {
+    recaptchaVerifier: any;
+    confirmationResult: any;
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
