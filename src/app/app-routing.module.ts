e682de526eef7900a3ded78ea3a5128fd18a62fd
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2023-04-02 20:50:54 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'de', loadChildren: () => import('./de/de.module').then(m => m.DeModule), title: 'FODS Delivery Exec' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
